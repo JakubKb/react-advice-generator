ea7076724e72472ee4631cf0d49d91fc79369ace
JustKooba <jakubsakw2009@gmail.com>
2023-08-23 23:06:33 +0200

    styled out the text

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AppContainer />
+    </div>
+  );
+}
+
+function AppContainer() {
+  return (
+    <div className="container">
+      <h1>Advice #117</h1>
+      <span className="p-wrapper">
+        <p>
+          "it is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </p>
+      </span>
+    </div>
+  );
+}
+
+export default App;
