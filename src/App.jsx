20a9e383f8e55eb3623ba50bd7b68171cf0109ea
JustKooba <jakubsakw2009@gmail.com>
2023-08-30 23:54:47 +0200

    Implemented api fetching, generating advice after clicking on a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import patternDivider from "./assets/pattern-divider-mobile.svg";
 import iconDice from "./assets/icon-dice.svg";
 
@@ -11,20 +12,34 @@ function App() {
 }
 
 function AppContainer() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdviceData();
+  }, []);
+
+  const fetchAdviceData = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+      })
+      .catch((error) => {
+        console.error("Error fetching advice:", error);
+      });
+  };
+
   return (
     <div className="container">
       <h1>Advice #117</h1>
       <span className="p-wrapper">
-        <p>
-          "it is easy to sit up and take notice, what's difficult is getting up
-          and taking action."
-        </p>
+        <p>{advice ?? "A piece of advice is loading..."}</p>
       </span>
       <span className="divider-wrapper">
-        <img src={patternDivider} className="divider" />
+        <img src={patternDivider} alt="Pattern Divider" className="divider" />
       </span>
       <span className="btn-wrapper">
-        <button className="dice-btn">
+        <button className="dice-btn" onClick={fetchAdviceData}>
           <img src={iconDice} alt="Dice icon" />
         </button>
       </span>
